1450445cb67e74ffb205898525d92411f7ddb1e7
Viktor Prikolota <VP_ampersand_P@icloud.com>
2020-01-21 20:21:11 +0300

    Initial Commit

---
+++ Smenka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smenka
+//
+//  Created by Viktor on 21.01.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
