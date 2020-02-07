7cd70d8a10b2792a5ab5c8245f82371729a9cc03
Viktor Prikolota <VP_ampersand_P@icloud.com>
2020-02-07 23:05:05 +0300

    Add action with staff and shiftType

--- Smenka/Manager/StorageManager.swift
+++ Smenka/Manager/StorageManager.swift
@@ -12,29 +12,78 @@ let realm = try! Realm()
 
 class StorageManager {
     
+    //   Action with scheduleShift
     static func saveScheduleShift(_ scheduleShifts: ScheduleShifts) {
         try! realm.write {
             realm.add(scheduleShifts)
         }
     }
-
-    // An error when deleting a shift schedule does not delete the shift list
+    
+    //  !!!An error when deleting a shift schedule does not delete the shift list!!!
     static func removeScheduleShift(_ scheduleShifts: ScheduleShifts) {
         try! realm.write {
             realm.delete(scheduleShifts)
         }
     }
-
+    
     static func editScheduleShift(_ scheduleShifts: ScheduleShifts, _ newScheduleShifts: ScheduleShifts) {
         try! realm.write {
             scheduleShifts.shifts = newScheduleShifts.shifts
         }
     }
     
+    
+    //   Action with shift
     static func editShift(_ shift: Shift, _ newShift: Shift) {
         try! realm.write {
             shift.shiftTypeIndex = newShift.shiftTypeIndex
             shift.shiftStaff = newShift.shiftStaff
         }
     }
+    
+    
+    //   Action with shiftType
+    static func saveShiftType(_ shiftType: ShiftType) {
+        try! realm.write {
+            realm.add(shiftType)
+        }
+    }
+    
+    static func removeShiftType(_ shiftType: ShiftType) {
+        try! realm.write {
+            realm.delete(shiftType)
+        }
+    }
+    
+    static func editShiftType(_ shiftType: ShiftType, _ newShiftType: ShiftType) {
+        try! realm.write {
+            shiftType.shiftTypeName = newShiftType.shiftTypeName
+            shiftType.shiftColor = newShiftType.shiftColor
+            shiftType.startTime = newShiftType.startTime
+            shiftType.endTime = newShiftType.endTime
+        }
+    }
+    
+    
+    //   Action with staff
+    static func saveStaff(_ staff: Staff) {
+        try! realm.write {
+            realm.add(staff)
+        }
+    }
+    
+    static func removeStaff(_ staff: Staff) {
+        try! realm.write {
+            realm.delete(staff)
+        }
+    }
+    
+    static func editStaff(_ staff: Staff, _ newStaff: Staff) {
+        try! realm.write {
+            staff.employeeName = newStaff.employeeName
+            staff.employeeSurname = newStaff.employeeSurname
+            staff.employeePosition = newStaff.employeePosition
+        }
+    }
+    
 }
