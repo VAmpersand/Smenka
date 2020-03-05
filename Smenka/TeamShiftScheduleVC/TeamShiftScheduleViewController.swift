a90736785e4d34ec23ff8e7422d05c889ac88ea4
Viktor Prikolota <VP_ampersand_P@icloud.com>
2020-03-05 23:16:48 +0300

    Create TeamShiftScheduleVC

---
+++ Smenka/TeamShiftScheduleVC/TeamShiftScheduleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TeamShiftScheduleViewController.swift
+//  Smenka
+//
+//  Created by Viktor on 05.03.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class TeamShiftScheduleViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
