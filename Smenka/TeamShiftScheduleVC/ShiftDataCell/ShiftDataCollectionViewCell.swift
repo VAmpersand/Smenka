d41ab006e92f6bacfe749bf323dcd1fda4ea75d3
Viktor Prikolota <VP_ampersand_P@icloud.com>
2020-03-09 23:47:25 +0300

    Added custom stuffDataCell

---
+++ Smenka/TeamShiftScheduleVC/ShiftDataCell/ShiftDataCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ShiftDataCollectionViewCell.swift
+//  Smenka
+//
+//  Created by Viktor on 09.03.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ShiftDataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var shiftTypeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
